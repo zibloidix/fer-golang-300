730ed3f38e92cabd666511bcbc36303056e45533
Aleksey Lomov <zibloidix@gmail.com>
2022-02-20 08:15:17 +1100

    DEV-93 1. Добавлен Dockerfile 2. Доабвлены переменные для переключения между продом и локальным сервером

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", runApp);
 
+const ENDPOINT = "http://95.143.188.122:3001/fer"
+if (window.__DEV__) {
+    "http://localhost:3001/fer"
+}
+
 const states = [
     "wellcome-to-service",
     "get-patient-data",
@@ -13,7 +18,6 @@ const states = [
     "cancel-appointment",
     "appointment-canceled-full-details"
 ]
-
 
 async function runApp() {
     const app = new Vue({
@@ -75,7 +79,7 @@ function executeTemplate(tmp, data) {
 }
 
 // Отправка запроса GetPatientInfo
-// await sendRequest("http://localhost:3001/fer", "GetPatientInfo", executeTemplate(GetPatientInfoRequest, getPatientData()))
+// await sendRequest(ENDPOINT, "GetPatientInfo", executeTemplate(GetPatientInfoRequest, getPatientData()))
 function getPatientData() {
     return {
         oms: "6555320880000082",
@@ -113,7 +117,7 @@ const GetPatientInfoRequest =
 
 
 // Отправка запроса GetMOInfoExtended
-// await sendRequest("http://localhost:3001/fer", "GetMOInfoExtended", GetMOInfoExtendedRequest)
+// await sendRequest(ENDPOINT, "GetMOInfoExtended", GetMOInfoExtendedRequest)
 const GetMOInfoExtendedRequest = 
 `<?xml version='1.0' encoding='UTF-8'?>
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
@@ -128,7 +132,7 @@ const GetMOInfoExtendedRequest =
 
 
 // Отправка запроса GetServiceSpecsInfo
-// await sendRequest("http://localhost:3001/fer", "GetServiceSpecsInfo", executeTemplate(GetServiceSpecsInfoRequest, getServiceSpecsInfoData()))
+// await sendRequest(ENDPOINT, "GetServiceSpecsInfo", executeTemplate(GetServiceSpecsInfoRequest, getServiceSpecsInfoData()))
 function getServiceSpecsInfoData() {
     return {
         moID: "194701"
@@ -150,7 +154,7 @@ const GetServiceSpecsInfoRequest =
 
 
 // Отправка запроса GetResourceInfo
-// await sendRequest("http://localhost:3001/fer", "GetResourceInfo", executeTemplate(GetResourceInfoRequest, getResourceInfoData()))
+// await sendRequest(ENDPOINT, "GetResourceInfo", executeTemplate(GetResourceInfoRequest, getResourceInfoData()))
 function getResourceInfoData() {
     return {
         serviceSpecID: "541 194701"
@@ -171,7 +175,7 @@ const GetResourceInfoRequest =
 </soapenv:Envelope>`
 
 // Отправка запроса GetScheduleInfo
-// await sendRequest("http://localhost:3001/fer", "GetScheduleInfo", executeTemplate(GetScheduleInfoRequest, getScheduleInfoData()))
+// await sendRequest(ENDPOINT, "GetScheduleInfo", executeTemplate(GetScheduleInfoRequest, getScheduleInfoData()))
 function getScheduleInfoData() {
     return {
         resourceID: "15350401926.59143",
@@ -201,7 +205,7 @@ const GetScheduleInfoRequest =
 
 
 // Отправка запроса CreateAppointment
-// await sendRequest("http://localhost:3001/fer", "CreateAppointment", executeTemplate(CreateAppointmentRequest, getCreateAppointmentData()))
+// await sendRequest(ENDPOINT, "CreateAppointment", executeTemplate(CreateAppointmentRequest, getCreateAppointmentData()))
 function getCreateAppointmentData() {
     return {
         slotID: "194701-59143-65959-61560-61620"
@@ -223,7 +227,7 @@ const CreateAppointmentRequest =
 
 
 // Отправка запроса CancelAppointment
-// await sendRequest("http://localhost:3001/fer", "CancelAppointment", executeTemplate(CancelAppointmentRequest, getCancelAppointmentData()))
+// await sendRequest(ENDPOINT, "CancelAppointment", executeTemplate(CancelAppointmentRequest, getCancelAppointmentData()))
 function getCancelAppointmentData() {
     return {
         bookIDMis: "05BDE828-E9D7-11EB-A253-005056AFD4A3"
@@ -287,317 +291,6 @@ function xmlToJson(xml) {
     return obj;
 }
 
-
-// Mockdata
-function getHospitalsMock() {
-    return [
-        {
-            "MO_Id": "194701",
-            "Reg_Phone": "(4242) 30-00-34",
-            "Organization_Name": "ГБУЗ Городская поликлиника №2 г.Южно-Сахалинска",
-            "Address_MO": "Южно-Сахалинск, пр-кт Мира, 85"
-        },
-        {
-            "MO_Id": "194702",
-            "Reg_Phone": "(4242) 70-50-91",
-            "Organization_Name": "ГБУЗ Городская поликлиника №2 г.Южно-Сахалинска Дальнее",
-            "Address_MO": "г.Южно-Сахалинск, с.Дальнее, ул.Новая, 20"
-        },
-        {
-            "MO_Id": "1943",
-            "Reg_Phone": "(4242) 72–21–09, (4242) 72–29–67",
-            "Organization_Name": "Сахалинский областной центр по профилактике и борьбе со СПИД",
-            "Address_MO": "Южно-Сахалинск, ул.Амурская, 53-А"
-        },
-        {
-            "MO_Id": "9494",
-            "Reg_Phone": "(4242) 510–710, (4242) 510–717, (4242) 510–718",
-            "Organization_Name": "Сахалинский областной кожно-венерологический диспансер",
-            "Address_MO": "Южно-Сахалинск, ул. Больничная, 46-Б"
-        },
-        {
-            "MO_Id": "65111101",
-            "Reg_Phone": "(4242) 72-84-99 - Регистратура",
-            "Organization_Name": "Городская травматологическая поликлиника",
-            "Address_MO": "Южно-Сахалинск, проспект Мира 56а/3"
-        },
-        {
-            "MO_Id": "78963",
-            "Reg_Phone": "(4242) 77-22-95 - Регистратура",
-            "Organization_Name": "Областная психиатрическая больница. Медосмотры",
-            "Address_MO": "Южно-Сахалинск, ул.Лермонтова, 110"
-        },
-        {
-            "MO_Id": "74125",
-            "Reg_Phone": "(4242) 76-08-82 - Регистратура",
-            "Organization_Name": "Областной наркологический диспансер. Медосмотры",
-            "Address_MO": "Южно-Сахалинск, ул.Горького, 12А"
-        }
-    ]
-}
-
-function getServicesMock() {
-    return [
-        {
-            "Service_Id": "1",
-            "Service_Name": "Врач-оториноларинголог"
-        },
-        {
-            "Service_Id": "2",
-            "Service_Name": "Врач-терапевт"
-        },
-        {
-            "Service_Id": "3",
-            "Service_Name": "Врач-стоматолог"
-        },
-    ]
-}
-
-function getResourcesMock() {
-    return [
-        {
-            "Resource_Id": "10",
-            "Resource_Name": "Хауз Грегори Михайлович"
-        },
-        {
-            "Resource_Id": "20",
-            "Resource_Name": "Робберт Чейз Иванович"
-        },
-        {
-            "Resource_Id": "30",
-            "Resource_Name": "Лиза Кадди Сергеевна"
-        },
-        {
-            "Resource_Id": "40",
-            "Resource_Name": "Эрик Форман Петрович"
-        },
-    ]
-}
-
-function getSlotsMock() {
-    return [
-        {
-            "Slot_Id": "194701-78825-66165-60300-61200",
-            "VisitTime": "2022-02-25T16:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66165-62100-63000",
-            "VisitTime": "2022-02-25T17:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66165-63000-63900",
-            "VisitTime": "2022-02-25T17:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66165-63900-64800",
-            "VisitTime": "2022-02-25T17:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66165-64800-65700",
-            "VisitTime": "2022-02-25T18:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66165-65700-66600",
-            "VisitTime": "2022-02-25T18:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-58500-59400",
-            "VisitTime": "2022-02-28T16:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-59400-60300",
-            "VisitTime": "2022-02-28T16:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-60300-61200",
-            "VisitTime": "2022-02-28T16:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-61200-62100",
-            "VisitTime": "2022-02-28T17:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-62100-63000",
-            "VisitTime": "2022-02-28T17:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-63900-64800",
-            "VisitTime": "2022-02-28T17:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-64800-65700",
-            "VisitTime": "2022-02-28T18:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66168-65700-66600",
-            "VisitTime": "2022-02-28T18:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-57600-58500",
-            "VisitTime": "2022-03-01T16:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-58500-59400",
-            "VisitTime": "2022-03-01T16:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-59400-60300",
-            "VisitTime": "2022-03-01T16:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-60300-61200",
-            "VisitTime": "2022-03-01T16:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-61200-62100",
-            "VisitTime": "2022-03-01T17:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-62100-63000",
-            "VisitTime": "2022-03-01T17:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-63000-63900",
-            "VisitTime": "2022-03-01T17:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-63900-64800",
-            "VisitTime": "2022-03-01T17:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-64800-65700",
-            "VisitTime": "2022-03-01T18:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66169-65700-66600",
-            "VisitTime": "2022-03-01T18:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-57600-58500",
-            "VisitTime": "2022-03-02T16:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-58500-59400",
-            "VisitTime": "2022-03-02T16:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-59400-60300",
-            "VisitTime": "2022-03-02T16:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-60300-61200",
-            "VisitTime": "2022-03-02T16:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-61200-62100",
-            "VisitTime": "2022-03-02T17:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-62100-63000",
-            "VisitTime": "2022-03-02T17:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-63000-63900",
-            "VisitTime": "2022-03-02T17:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-63900-64800",
-            "VisitTime": "2022-03-02T17:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-64800-65700",
-            "VisitTime": "2022-03-02T18:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66170-65700-66600",
-            "VisitTime": "2022-03-02T18:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-57600-58500",
-            "VisitTime": "2022-03-03T16:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-58500-59400",
-            "VisitTime": "2022-03-03T16:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-59400-60300",
-            "VisitTime": "2022-03-03T16:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-60300-61200",
-            "VisitTime": "2022-03-03T16:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-61200-62100",
-            "VisitTime": "2022-03-03T17:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-62100-63000",
-            "VisitTime": "2022-03-03T17:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-63000-63900",
-            "VisitTime": "2022-03-03T17:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-63900-64800",
-            "VisitTime": "2022-03-03T17:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-64800-65700",
-            "VisitTime": "2022-03-03T18:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66171-65700-66600",
-            "VisitTime": "2022-03-03T18:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-57600-58500",
-            "VisitTime": "2022-03-04T16:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-58500-59400",
-            "VisitTime": "2022-03-04T16:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-59400-60300",
-            "VisitTime": "2022-03-04T16:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-60300-61200",
-            "VisitTime": "2022-03-04T16:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-61200-62100",
-            "VisitTime": "2022-03-04T17:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-62100-63000",
-            "VisitTime": "2022-03-04T17:15:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-63000-63900",
-            "VisitTime": "2022-03-04T17:30:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-63900-64800",
-            "VisitTime": "2022-03-04T17:45:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-64800-65700",
-            "VisitTime": "2022-03-04T18:00:00.000"
-        },
-        {
-            "Slot_Id": "194701-78825-66172-65700-66600",
-            "VisitTime": "2022-03-04T18:15:00.000"
-        }
-    ].map( s => {
-        s.VisitTime = new Date(s.VisitTime)
-        return s
-    })
-}
-
 function getAppointmentHistoryList() {
     return [
         {
@@ -656,12 +349,12 @@ function getAppointmentHistoryList() {
 }
 
 async function getPatientID() {
-    const resp = await sendRequest("http://localhost:3001/fer", "GetPatientInfo", executeTemplate(GetPatientInfoRequest, getPatientData()))
+    const resp = await sendRequest(ENDPOINT, "GetPatientInfo", executeTemplate(GetPatientInfoRequest, getPatientData()))
     return resp["soapenv:Envelope"]["soapenv:Body"]["er:GetPatientInfoResponse"].Patient_Id
 }
 
 async function getHospitals() {
-    const resp = await sendRequest("http://localhost:3001/fer", "GetMOInfoExtended", GetMOInfoExtendedRequest)
+    const resp = await sendRequest(ENDPOINT, "GetMOInfoExtended", GetMOInfoExtendedRequest)
     const hospitals = resp["soapenv:Envelope"]["soapenv:Body"]["er:GetMOInfoExtendedResponse"].ListMO.MO.map( m => {
         return {
             MO_Id: m.MO_Id,
@@ -675,7 +368,7 @@ async function getHospitals() {
 }
 
 async function getServices() {
-    const resp = await sendRequest("http://localhost:3001/fer", "GetServiceSpecsInfo", executeTemplate(GetServiceSpecsInfoRequest, getServiceSpecsInfoData()))
+    const resp = await sendRequest(ENDPOINT, "GetServiceSpecsInfo", executeTemplate(GetServiceSpecsInfoRequest, getServiceSpecsInfoData()))
     const services = resp["soapenv:Envelope"]["soapenv:Body"]["er:GetServiceSpecsInfoResponse"].ListServiceSpecs.ServiceSpec.map( s => {
         return {
             Service_Id: s.ServiceSpec_Id,
@@ -686,13 +379,13 @@ async function getServices() {
 }
 
 async function getResources() {
-    const resp = await sendRequest("http://localhost:3001/fer", "GetResourceInfo", executeTemplate(GetResourceInfoRequest, getResourceInfoData()))
+    const resp = await sendRequest(ENDPOINT, "GetResourceInfo", executeTemplate(GetResourceInfoRequest, getResourceInfoData()))
     const resources = resp["soapenv:Envelope"]["soapenv:Body"]["er:GetResourceInfoResponse"].ListResource.Resource
     return resources
 }
 
 async function getSlots() {
-    const resp = await sendRequest("http://localhost:3001/fer", "GetScheduleInfo", executeTemplate(GetScheduleInfoRequest, getScheduleInfoData()))
+    const resp = await sendRequest(ENDPOINT, "GetScheduleInfo", executeTemplate(GetScheduleInfoRequest, getScheduleInfoData()))
     const slots = resp["soapenv:Envelope"]["soapenv:Body"]["er:GetScheduleInfoResponse"].Schedule.Slots.map(s => {
         s.VisitTime = dateToString( s.VisitTime )
         return s
@@ -701,7 +394,7 @@ async function getSlots() {
 }
 
 async function getCreateAppointment() {
-    const resp = await sendRequest("http://localhost:3001/fer", "CreateAppointment", executeTemplate(CreateAppointmentRequest, getCreateAppointmentData()))
+    const resp = await sendRequest(ENDPOINT, "CreateAppointment", executeTemplate(CreateAppointmentRequest, getCreateAppointmentData()))
     const slot = resp["soapenv:Envelope"]["soapenv:Body"]["er:CreateAppointmentResponse"]
     const { Book_Id_Mis, Comment, Session_ID, Slot_Id, Status_Code, VisitTime } = slot
     return {
